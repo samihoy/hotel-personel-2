943cc16fb782d1d40a79e7d0ec69f2fbb455dc67
Bror Lundberg <maximillian.lundber@chasacademy.se>
2024-10-15 14:51:42 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_personel_2
+{
+    internal class Class1
+    {
+
+    }
+    public class Person
+    {
+        public Person(string name, int age, int iD, DateTime startDate, double salary, string department)
+        {
+            this.name = name;
+            this.age = age;
+            ID = iD;
+            this.startDate = startDate;
+            this.salary = salary;
+            this.department = department;
+        }
+        public string name { get; set; }
+        public int age { get; set; }
+        public int ID { get; set; }
+        public DateTime startDate { get; set; }
+        public double salary { get; set; }
+        public string department { get; set; }
+
+        public virtual void PersonalInfo()
+        {
+            Console.WriteLine($"Employment informatiom \n {name}\n {age} \n {ID} och \n {salary}");
+        }
+        public virtual void Introduce()
+        {
+            Console.WriteLine($"Jag heter \n {name} och är {age} gammal.");
+        }
+        public virtual void Work()
+        {
+            Console.WriteLine($"\n{name} gör sitt jobb och arbetar i {department} avdelningen\n");
+        }
+    }
+    public class Manager : Person
+    {        
+        public Manager(string name, int age, int iD, DateTime startDate, double salary, string department) : base(name, age, iD, startDate, salary, department)
+        {
+            
+        }
+        public virtual void HoldMeeting()
+        {
+            base.Introduce(); // base to call the parent for information 
+            Console.WriteLine($"{name} är i ett möte");
+        }
+        public virtual void PlanBudget()
+        {
+            base.Work();
+            Console.WriteLine($"{name} planerar budgeten");
+        }
+    }
+    public class Employee : Manager
+    {
+        public string jobtitle { get; set; }
+        public Employee(string name, int age, int iD, DateTime startDate, double salary, string department, string jobtitle) : base(name, age, iD, startDate, salary, department)
+        {
+            this.jobtitle = jobtitle;
+        }
+        public override void Work()
+        {
+            base.Work();
+            Console.WriteLine($"{name} arbetar som {jobtitle} och gör sitt jobb");
+        }
+    }
+    public class Consult : Person
+    {
+        public double fee { get; set; }
+        public string company { get; set; }
+
+        public string expertise { get; set; }
+
+        public Consult(string name, int age, int iD, DateTime startDate, double salary, double fee, string company, string expertise, string department) : base(name, age, iD, startDate, salary, department)
+        {
+            this.fee = fee;
+            this.company = company;
+            this.expertise = expertise;
+        }
+        public void GiveAdvice()
+        {
+
+            Console.WriteLine($"{name} ger råd hur dom kan förbättra sin {expertise} för {fee} i timmen");
+        }
+    }
+    public class Housekeeper: Employee
+    {
+        public Housekeeper(string name, int age, int iD, DateTime startDate, double salary, string department, string jobtitle) : base(name, age, iD, startDate, salary, department,jobtitle)
+        {
+
+        }
+        public override void Work()
+        {
+            base.Work(); 
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_personel_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personallista2 = new List<Person>();
+
+            List<Person> personallista = new List<Person>
+            {   new Person("Erik", 35, 298492, new DateTime(2019, 12, 04), 46000, "basement people"),
+                new Manager("Bossman", 67, 234492, new DateTime(2019, 12, 04), 36000, "managment"),
+                new Employee("Eva", 30, 267692, new DateTime(2019, 12, 04), 36000, "managment", "receptionist"),
+                new Consult("Anna", 23, 234292, new DateTime(2019, 12, 04), 36000, 1500,"no department", "Hotell Experterna AB", "logistik"),
+                new Housekeeper("Elin", 32, 26764, new DateTime(2019, 12, 04), 36000, "cleaning", "cleaner")
+
+            };
+
+            Person anställdx = new Person("Erik", 35, 267492, new DateTime(2019, 12, 04), 36.000, "basement people");
+ 
+            Manager chef = new Manager("Bossman", 67, 267492, new DateTime(2019, 12, 04), 36.000, "managment");
+
+            Employee anstäldz = new Employee("Eva", 30, 267492, new DateTime(2019, 12, 04), 36.000, "managment", "receptionist");
+            
+            Consult anställdv = new Consult("Anna", 23, 267492, new DateTime(2019, 12, 04), 36.000, 1500,"no department" ,"Hotell Experterna AB", "logistik");
+            
+            Housekeeper anstäldj = new Housekeeper("Elin", 32, 267492, new DateTime(2019, 12, 04), 36.000, "cleaning", "cleaner");
+            
+
+            personallista2.Add(anställdx);
+            personallista2.Add(chef);
+            personallista2.Add(anstäldz);
+            personallista2.Add(anställdv);
+            personallista2.Add(anstäldj);
+
+            foreach (var item in personallista)
+            {
+
+                item.Work();
+            }
+            anställdv.GiveAdvice();
+
+            double avrageage = personallista.Average(x=>x.age);
+            double avragesalery = personallista.Average(y => y.salary);
+            Console.WriteLine($"{avrageage} is the avrage age of your emploeys");
+            Console.WriteLine($"{avragesalery} is the avrage salery of your emploeys");
+
+
+
+
+            double i = 0;
+            double z = 0;
+
+            foreach (var item in personallista)
+            {
+                i++;
+                z += item.age;
+
+            }
+
+            double yy = z / i;
+            Console.WriteLine(yy);
+
+
+
+
+
+
+        }
+    }
+}
